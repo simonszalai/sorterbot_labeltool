b9ed33013373f3058a11453d0b13b652bb9773f5
Simon Szalai <simon.szalai@gmail.com>
2020-03-10 11:21:57 +0100

    box movements calculated but not verified

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import cv2
 import sys
 from player import Player
 
@@ -6,22 +5,9 @@ from player import Player
 class Main:
     def __init__(self):
         self.video_path = sys.argv[1]
-        self.player = Player(self.video_path, self.onMouseClick)
+        self.player = Player(self.video_path)
         self.player.start()
-        self.downX = None
-        self.downY = None
-
-    def onMouseClick(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.downX = x
-            self.downY = y
-            print("Coordinates on MouseDown: X: ", x, "Y: ", y)
-        if event == cv2.EVENT_LBUTTONUP:
-            self.player.rectangles.append(((self.downX, self.downY), (x, y)))
-            print("Coordinates on MouseUp: X: ", x, "Y: ", y)
 
 
 if __name__ == "__main__":
     Main()
-
-
--- player.py
+++ player.py
@@ -1,23 +1,24 @@
 import cv2
-
-
-def flick(x):
-    pass
+import math
 
 
 class Player:
-    def __init__(self, video_path, onMouseClick):
+    def __init__(self, video_path):
         # Initialize variables
         self.window = "window"
+        self.radius = 800
+        self.max_degrees = 160.0
         self.frame = None
         self.status = "stay"
         self.tracker_position = 0
-        self.rectangles = []
+        self.rectangles = [((300, 400), (500, 600), 0)]
+        self.downX = None
+        self.downY = None
 
         # Set up Video Window
         cv2.namedWindow(self.window, flags=cv2.WINDOW_GUI_NORMAL + cv2.WINDOW_AUTOSIZE)
         cv2.moveWindow(self.window, 250, 150)
-        cv2.setMouseCallback(self.window, onMouseClick)
+        cv2.setMouseCallback(self.window, self.onMouseClick)
 
         # Create video capture object
         self.video = cv2.VideoCapture(video_path)
@@ -26,14 +27,54 @@ class Player:
         self.total_frames = self.video.get(cv2.CAP_PROP_FRAME_COUNT)
 
         # Set up trackbar for player position
-        cv2.createTrackbar("P", self.window, 0, int(self.total_frames) - 1, flick)
+        cv2.createTrackbar("P", self.window, 0, int(self.total_frames) - 1, self.seek)
         cv2.setTrackbarPos("P", self.window, 0)
+
+    def onMouseClick(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.downX = x
+            self.downY = y
+        if event == cv2.EVENT_LBUTTONUP:
+            self.rectangles.append(((self.downX, self.downY), (x, y), self.tracker_position))
+
+    def get_new_position(self, old_rect_points):
+        half_w = self.video.get(cv2.CAP_PROP_FRAME_WIDTH) / 2
+        half_h = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT) / 2
+
+        old_tracker_position = old_rect_points[2]
+
+        angle_old = self.max_degrees * old_tracker_position / self.total_frames
+        angle_new = self.max_degrees * self.tracker_position / self.total_frames
+
+        box_w = old_rect_points[1][0] - old_rect_points[0][0]
+        box_h = old_rect_points[1][1] - old_rect_points[0][1]
+
+        x_old = old_rect_points[0][0]
+        y_old = old_rect_points[0][1]
+
+        # Calculate old point coordinates in polar system
+        gamma_old = math.atan((half_w - x_old) / (self.radius + half_h - y_old + 0.000001))
+        gamma_old_deg = math.degrees(gamma_old)
+        polar_radius = (half_w - x_old) / (math.sin(gamma_old) + 0.000001)
+
+        # Calculate new gamma
+        gamma_new_deg = gamma_old_deg - (angle_new - angle_old)
+        gamma_new = math.radians(gamma_new_deg)
+
+        # Calculate new point coordinates in Cartesian system
+        x_new = half_w - math.sin(gamma_new) * polar_radius
+        y_new = self.radius + half_h - (math.cos(gamma_new) * polar_radius)
+
+        return [(int(x_new), int(y_new)), (int(x_new) + box_w, int(y_new) + box_h)]
+
+    def seek(self, new_tracker_position):
+        self.tracker_position = new_tracker_position
 
     def start(self):
         while True:
             try:
                 # Jump to start when last frame is reached
-                if self.tracker_position == self.total_frames - 1:
+                if self.tracker_position == self.total_frames:
                     self.tracker_position = 0
 
                 # Set video to new frame
@@ -42,8 +83,11 @@ class Player:
                 # Grab current frame
                 frame_grab_success, self.frame = self.video.read()
 
+                # Calculate current ractangle positions
+                rectangles = [self.get_new_position(rectangle) for rectangle in self.rectangles]
+
                 # Draw rectangles
-                for rectangle in self.rectangles:
+                for rectangle in rectangles:
                     cv2.rectangle(
                         self.frame,
                         rectangle[0],
@@ -68,6 +112,7 @@ class Player:
                 }[cv2.waitKey(10)]
 
                 if self.status == "play":
+                    # self.update_reactangles(self.tracker_position + 1)
                     self.tracker_position += 1
                     cv2.setTrackbarPos("P", self.window, self.tracker_position)
                     continue
@@ -80,11 +125,13 @@ class Player:
                     break
 
                 if self.status == "prev_frame":
+                    # self.update_reactangles(self.tracker_position - 1)
                     self.tracker_position -= 1
                     cv2.setTrackbarPos("P", self.window, self.tracker_position)
                     self.status = "stay"
 
                 if self.status == "next_frame":
+                    # self.update_reactangles(self.tracker_position + 1)
                     self.tracker_position += 1
                     cv2.setTrackbarPos("P", self.window, self.tracker_position)
                     self.status = "stay"
