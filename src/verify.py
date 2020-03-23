2de8e661cd99bb83b175c7c30615632c6653f2ea
Simon Szalai <simon.szalai@gmail.com>
2020-03-23 12:51:54 +0100

    VERIFY: documentation added

--- src/verify.py
+++ src/verify.py
@@ -1,3 +1,16 @@
+"""
+The Verify module provides a way to manually verify every picture in the dataset. When a higher-end robotic arm is used which is capable of very accure and
+consistent movement, this modue might not be needed, but for a low-end arm, driven with servos, a relatively high number of images might end up blurred
+or the objects out of their bounding boxes due to inconsistent movements of the arm. To avoid these ending up in the final dataset, this module is used.
+It can process multiple export folders at once. The IDs for these folders has to be provided at startup. After all the files in these folder are read in,
+a UI will appear showing the first picture and the generated bounding boxes over the objects. If the picture looks alright, press Num+, and it will be added
+to the dataset. If a picture was added by mistake, press Num-, and the last picture will be removed and show again to revise the decision. If any other key
+is pressed (Num Enter conveniently), the current picture is skipped and the next one is shown. After every picture has been added or skipped, the images
+will be copied to the dataset folder (divided into train and validation set) and one JSON file will be created for the training and validation sets.
+At the end of the process, the generated dataset will be uploaded to the datasets S3 bucket.
+
+"""
+
 import os
 import sys
 import cv2
@@ -9,6 +22,11 @@ from storage import Storage
 
 
 class Verify:
+    """
+    Verify class to contain functions which verify and combine multiple exports into one dataset.
+
+    """
+
     def __init__(self):
         cv2.namedWindow("window", flags=cv2.WINDOW_GUI_NORMAL + cv2.WINDOW_AUTOSIZE)
         cv2.moveWindow("window", 250, 150)
@@ -18,12 +36,29 @@ class Verify:
         self.datasets_path = os.path.join(Path().parent.absolute(), "datasets")
         self.verified_data = []
 
-    def run(self):
+    def run(self, manual_verification=True):
+        """
+        Function to run the verification process.
+
+        Parameters
+        ----------
+        manual_verification : bool
+            Flag to manually verify images or just generate JSON and upload it from previosly verified images that
+            were already copied to the dataset folder.
+
+        """
+
         self.load_json()
-        # self.verify_images()
-        self.export_to_dataset()
+        if manual_verification:
+            self.verify_images()
+        self.export_to_dataset(manual_verification=manual_verification)
 
     def load_json(self):
+        """
+        Function that loads every JSON file in the specified exports folders and loads the data stored in them.
+
+        """
+
         self.all_data = []
         for export_id in self.export_ids:
             exports_path = os.path.join(Path().parent.absolute(), "exports", export_id)
@@ -33,6 +68,12 @@ class Verify:
                     self.all_data += json.load(json_file)
 
     def verify_images(self):
+        """
+        Function that loops through the images from the loaded JSON files, shows them on the screen one by one, and based on the user input,
+        saves them to the dataset or skips them.
+
+        """
+
         i = 0
         actions = {}
         while True:
@@ -67,6 +108,22 @@ class Verify:
                 i += 1
 
     def display_image(self, img):
+        """
+        Shows the provided image on screen and draws the bounding boxes on it.
+
+        Parameters
+        ----------
+        img : object
+            Img object from the loaded JSON file. Contains the full path to the image under "file_name" key and
+            the annotations including bounding boxes under "annotations" key.
+
+        Returns
+        -------
+        key_code : int
+            Code of the pressed key.
+
+        """
+
         # Get frame
         frame = cv2.imread(img["file_name"])
         frame = cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_AREA)
@@ -90,15 +147,30 @@ class Verify:
 
         return key_code
 
-    def export_to_dataset(self, files_already_copied=True, upload_only_json=False):
+    def export_to_dataset(self, manual_verification, train_ratio=0.8):
+        """
+        A function that aggregates images from one more more exports into a single dataset. There ware 2 ways to get which images from an export should be kept:
+        From user input or from images already copied to the dataset folder. The second way is useful for development, when clicking though the images one by one
+        is better avoided. The "file_name" for every image are updated to relative paths, they are devided into train and validation sets, copied to the dataset
+        folder and finally uploaded to S3.
+
+        Parameters
+        ----------
+        manual_verification : bool
+            Flag to enable manual verification. Disabling it results in loading data from images copied to dataset folder. Useful for development.
+        train_ratio : float
+            Defines the proportion of the train and validation sets. E.g.: train_ratio=0.8 ==> ~80% of images to train set, ~20% to validation set.
+
+        """
+
         # Construct dataset path and create folder if necessary
         dataset_folder = "-".join(self.export_ids)
         dataset_path = os.path.join(self.datasets_path, dataset_folder)
         os.makedirs(os.path.join(dataset_path, "train"), exist_ok=True)
         os.makedirs(os.path.join(dataset_path, "val"), exist_ok=True)
 
-        if files_already_copied:
-            # Get verified data from self.all_data by searching for each file already copied to dataset
+        if manual_verification:
+            # Instead of user input, get verified data from self.all_data by searching for each file already copied to dataset
             verified_data = []
             for root, _, files in os.walk(dataset_path):
                 for file in files:
@@ -113,7 +185,7 @@ class Verify:
         data = {"train": [], "val": []}
         for img in verified_data:
             # Decide about each image if it goes to train or val dataset
-            set_type = "train" if 0.8 > random.random() else "val"
+            set_type = "train" if train_ratio > random.random() else "val"
 
             # Copy image file to dataset folder (train or val subfolder)
             dest_path = os.path.join(dataset_path, set_type, os.path.basename(img["file_name"]))
@@ -124,9 +196,9 @@ class Verify:
             img["file_name"] = os.path.join(os.path.basename(dataset_path), set_type, os.path.basename(img["file_name"]))
             data["train"].append(img) if set_type == "train" else data["val"].append(img)
 
-        # Write annotations JSON file for train and val datasets
+        # Write annotations JSON file for train and val datasets and upload dataset
         for data_type in data:
-            self.storage.upload_dataset(dataset_path=os.path.join(dataset_path, data_type), only_json=upload_only_json)
+            self.storage.upload_dataset(dataset_path=os.path.join(dataset_path, data_type), only_json=manual_verification)
             with open(os.path.join(dataset_path, data_type, "annotations.json"), "w") as outfile:
                 json.dump(data[data_type], outfile)
 
