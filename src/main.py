931707371264f5b1d0023db3faa56b92f86b4ffe
Simon Szalai <simon.szalai@gmail.com>
2020-03-23 12:52:48 +0100

    main.py simplified

--- src/main.py
+++ src/main.py
@@ -5,21 +5,15 @@ from player import Player
 from storage import Storage
 
 
-class Main:
-    def __init__(self):
-        self.storage = Storage()
-        self.recording_set_id = sys.argv[1]
-        self.videos_path = os.path.join(Path().parent.absolute(), "videos", self.recording_set_id)
-        self.storage.download_videos(videos_path=self.videos_path)
+storage = Storage()
+recording_set_id = sys.argv[1]
+videos_path = os.path.join(Path().parent.absolute(), "videos", recording_set_id)
+storage.download_videos(videos_path=videos_path)
 
-        if not os.path.isdir(self.videos_path):
-            os.makedirs(self.videos_path, exist_ok=True)
+if not os.path.isdir(videos_path):
+    os.makedirs(videos_path, exist_ok=True)
 
-        avi_files = [f.path for f in os.scandir(self.videos_path) if os.path.splitext(f.name)[1] == ".avi"]
+avi_files = [f.path for f in os.scandir(videos_path) if os.path.splitext(f.name)[1] == ".avi"]
 
-        for avi_file in avi_files:
-            Player(avi_file, export_id=self.recording_set_id).start()
-
-
-if __name__ == "__main__":
-    Main()
+for avi_file in avi_files:
+    Player(avi_file, export_id=recording_set_id).start()
